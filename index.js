18af7978142ff7871858d4f42ba4a845f2288d56
shin4x <dg.dimon2003@gmail.com>
2022-11-26 20:21:26 +0300

    ADD JS

---
+++ index.js
@@ -0,0 +1,27 @@
+
+
+const calc = document.querySelector( '.calc');
+const result = document.querySelector ('.screen')
+
+calc.addEventListener('click', function(event){
+
+if(!event.target.classList.contains ('btn')) return;
+
+const value = event.target.innerText;
+
+
+switch(value){
+    case 'AC':
+        result. innerText = '';
+        break;
+ 
+    case '=':
+        result.innerText = eval(result.innerText).toFixed(2);
+        break;
+    
+    default:
+        result.innerText += value;
+
+}
+});
+
